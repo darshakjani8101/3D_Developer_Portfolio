42d112bb10a6bf08243d38704707970866a45a4c
Darshak Jani <darshakjani8101@gmail.com>
2024-03-20 17:24:18 +0530

    Designed about section and implemented higher order section wrapper component.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,10 +26,10 @@ const Hero = () => {
 
       <ComputersCanvas />
 
-      <div className="absolute xs:bottom-5 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute xs:bottom-5 bottom-10 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-            <motion.dev
+            <motion.div
               animate={{ y: [0, 24, 0] }}
               transition={{
                 duration: 1.5,
